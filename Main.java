33b4bbc0863a85dfc09018dcad29155630eff77f
Siddesh <siddharthgs384@gmail.com>
2025-09-22 19:34:40 +0530

    third

---
+++ Main.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.LinkedHashSet;
+
+public class Main {
+	public static void main(String[] args) {
+		LinkedHashSet h= new LinkedHashSet();
+		h.add("Yash");
+		h.add("Siddu");
+		h.add("Yash");
+		h.add("Madhu");
+		h.add("Darshi");
+		h.add("Vigni");
+		System.out.println(h);
+	}
+
+}
